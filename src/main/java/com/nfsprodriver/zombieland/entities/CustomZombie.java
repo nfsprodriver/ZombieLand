d822140f1f6edee9fd2c20a79c76acc79ecad9f2
Bjarne Roß <bjarne@brserv.de>
2021-04-04 01:54:49 +0200

    Custom drops

--- src/main/java/com/nfsprodriver/zombieland/entities/CustomZombie.java
+++ src/main/java/com/nfsprodriver/zombieland/entities/CustomZombie.java
@@ -1,14 +1,17 @@
 package com.nfsprodriver.zombieland.entities;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Zombie;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.metadata.FixedMetadataValue;
-import org.bukkit.metadata.MetadataValue;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomZombie {
     private final Zombie zombie;
@@ -24,15 +27,31 @@ public class CustomZombie {
     public Zombie createZombie1() {
         NamespacedKey gameNameKey = new NamespacedKey(plugin, "gameName");
         zombie.getPersistentDataContainer().set(gameNameKey, PersistentDataType.STRING, gameName);
+        zombie.setBaby(false);
         EntityEquipment equipment = zombie.getEquipment();
         assert equipment != null;
         ItemStack helmet = new ItemStack(Material.IRON_HELMET);
         ItemStack mainHand = new ItemStack(Material.IRON_SWORD);
         equipment.setHelmet(helmet);
-        equipment.setHelmetDropChance(0.0F);
         equipment.setItemInMainHand(mainHand);
-        equipment.setItemInMainHandDropChance(0.2F);
+        Map<ItemStack, Float> dropsMap = new HashMap<ItemStack, Float>(); //Float is for probability from 0.0 to 1.0
+        dropsMap.put(new ItemStack(Material.BREAD, 3), 0.3F);
+        NamespacedKey customDropsKey = new NamespacedKey(plugin, "customDrops");
+        zombie.getPersistentDataContainer().set(customDropsKey, PersistentDataType.STRING, mapToString(dropsMap));
 
         return zombie;
+    }
+
+    private String mapToString(Map<ItemStack, Float> map) {
+        JsonArray json = new JsonArray();
+        map.forEach((itemStack, prob) -> {
+            JsonObject item = new JsonObject();
+            item.addProperty("material", itemStack.getType().name());
+            item.addProperty("amount", itemStack.getAmount());
+            item.addProperty("prob", prob);
+            json.add(item.toString());
+        });
+
+        return json.toString();
     }
 }
--- src/main/java/com/nfsprodriver/zombieland/events/ZombieKilled.java
+++ src/main/java/com/nfsprodriver/zombieland/events/ZombieKilled.java
@@ -1,13 +1,21 @@
 package com.nfsprodriver.zombieland.events;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Score;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ZombieKilled implements Listener {
     private JavaPlugin plugin;
@@ -19,6 +27,7 @@ public class ZombieKilled implements Listener {
     @EventHandler
     public void onZombieKilled(EntityDeathEvent event) {
         NamespacedKey gameNameKey = new NamespacedKey(plugin, "gameName");
+        event.getDrops().clear();
         String gameName = event.getEntity().getPersistentDataContainer().get(gameNameKey, PersistentDataType.STRING);
         assert gameName != null;
         if (event.getEntity().getKiller() != null) {
@@ -26,5 +35,32 @@ public class ZombieKilled implements Listener {
             Score score = player.getScoreboard().getObjective("zombieland" + gameName).getScore(player.getName() + " kills");
             score.setScore(score.getScore() + 1);
         }
+        NamespacedKey dropsStringKey = new NamespacedKey(plugin, "customDrops");
+        String dropsString = event.getEntity().getPersistentDataContainer().get(dropsStringKey, PersistentDataType.STRING);
+        assert dropsString != null;
+        Map<ItemStack, Float> dropsMap = stringToMap(dropsString);
+        dropsMap.forEach(((itemStack, prob) -> {
+            if (getRandomBoolean(prob)) {
+                event.getDrops().add(itemStack);
+            }
+        }));
+    }
+
+    private Map<ItemStack, Float> stringToMap(String jsonString) {
+        JsonArray json = new JsonParser().parse(jsonString).getAsJsonArray();
+        Map<ItemStack, Float> map = new HashMap<ItemStack, Float>();
+        for (int i = 0; i < json.size(); i++) {
+            JsonObject item = new JsonParser().parse(json.get(i).getAsString()).getAsJsonObject();
+            ItemStack itemStack = new ItemStack(Material.getMaterial(item.get("material").getAsString()), item.get("amount").getAsInt());
+            Float prob = item.get("prob").getAsFloat();
+            map.put(itemStack, prob);
+        }
+
+        return map;
+    }
+
+    static boolean getRandomBoolean(float probability) {
+        double randomValue = Math.random();
+        return randomValue <= probability;
     }
 }
